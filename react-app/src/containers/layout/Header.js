273c81fa0c2d07a6886747523d04036e5eec3f41
king-krs-one <jonathan.nickels@googlemail.com>
2023-07-27 21:35:58 +0200

    refine style fore header menu and side panel

--- react-app/src/containers/layout/Header.js
+++ react-app/src/containers/layout/Header.js
@@ -6,22 +6,25 @@ import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/images/logo.png'
 import LogoText from '../../assets/images/logo_text.png'
+import LogoTextWhite from '../../assets/images/logo_text_white.png'
 
 function Header(props) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const { navigation } = props
 
   return (
-    <header className="sticky inset-x-0 top-0 z-50">
-      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
+    <header className="App-header sticky inset-x-0 top-0 z-50">
+      <nav className="flex items-center justify-between px-4 py-2 lg:px-4" aria-label="Global">
+        {/* Header Logo */}
+        <div className="App-logo flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Garden of Chats</span>
             <img className="h-10 w-auto inline-block" src={Logo} alt="" />
-            <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoText} alt="" />
+            <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoTextWhite} alt="" />
           </a>
         </div>
-        <div className="flex lg:hidden">
+        {/* Panel Menu Open */}
+        <div className="App-panel-open flex lg:hidden">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -31,12 +34,14 @@ function Header(props) {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        {/* Header Menu */}
+        <div className="App-menu hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
             <Link key={item.name} to={item.href} className='menuItem text-sm font-semibold leading-6'>{item.name}</Link>
           ))}
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        {/* App Profile */}
+        <div className="App-profile hidden lg:flex lg:flex-1 lg:justify-end">
           <Link to="/login" className='menuItem text-sm font-semibold leading-6'>Sign in</Link>
         </div>
       </nav>
@@ -50,34 +55,36 @@ function SidePanel(props) {
   const { navigation, isOpen, setIsOpen } = props
 
   return (
-    <Dialog as="div" className="lg:hidden" open={isOpen} onClose={setIsOpen}>
+    <Dialog as="div" className="App-panel lg:hidden" open={isOpen} onClose={setIsOpen}>
       <div className="fixed inset-0 z-50" />
-      <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div className="flex items-center justify-between">
-          <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-10 w-auto inline-block" src={Logo} alt="" />
-            <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoText} alt="" />
-          </a>
-          <button
-            type="button"
-            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            onClick={() => setIsOpen(false)}
-          >
-            <span className="sr-only">Close menu</span>
-            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="mt-6 flow-root">
-          <div className="-my-6 divide-y divide-gray-500/10">
-            <div className="space-y-2 py-6">
-              {navigation.map((item) => (
-                <Link key={item.name} to={item.href} className='menuItem block text-base font-semibold leading-7'>{item.name}</Link>
-              ))}
-            </div>
-            <div className="py-6">
-              <Link to="/login" className='menuItem block text-base font-semibold leading-7'>Sign In</Link>
-            </div>
+      <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-4 py-4 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="App-panel-header flex items-end justify-between">
+          {/* Panel Close */}
+          <div className="App-panel-logo flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Garden of Chats</span>
+              <img className="h-10 w-auto inline-block" src={Logo} alt="" />
+              <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoText} alt="" />
+            </a>
           </div>
+          <div className="App-panel-close">
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        <hr className='divider' />
+        <div className="App-panel-menu flow-root">
+          {navigation.map((item) => (
+            <Link key={item.name} to={item.href} className='menuItem'>{item.name}</Link>
+          ))}
+          <hr className='divider' />
+          <Link to="/login" className='menuItem'>Login</Link>
         </div>
       </Dialog.Panel>
     </Dialog>
