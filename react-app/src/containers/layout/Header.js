2b8bed49d65ec75336608cc42165ced6f9fbd372
king-krs-one <jonathan.nickels@googlemail.com>
2023-07-27 19:40:36 +0200

    adding assets folder and logo

--- react-app/src/containers/layout/Header.js
+++ react-app/src/containers/layout/Header.js
@@ -3,21 +3,19 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
+import Logo from '../../assets/images/logo.png'
+
 function Header(props) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const { navigation } = props
-  
+
   return (
     <header className="sticky inset-x-0 top-0 z-50">
       <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
+            <img className="h-8 w-auto" src={Logo} alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -49,11 +47,7 @@ function Header(props) {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <img className="h-8 w-auto" src={Logo} alt="" />
             </a>
             <button
               type="button"
