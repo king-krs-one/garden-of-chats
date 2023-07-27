de2ca6b692bddc4edabd4190510a23a6560e25a8
king-krs-one <jonathan.nickels@googlemail.com>
2023-07-27 20:28:50 +0200

    add logo text and include in header/menu

--- react-app/src/containers/layout/Header.js
+++ react-app/src/containers/layout/Header.js
@@ -5,6 +5,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/images/logo.png'
+import LogoText from '../../assets/images/logo_text.png'
 
 function Header(props) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -16,7 +17,8 @@ function Header(props) {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src={Logo} alt="" />
+            <img className="h-10 w-auto inline-block" src={Logo} alt="" />
+            <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoText} alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -53,7 +55,8 @@ function SidePanel(props) {
       <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div className="flex items-center justify-between">
           <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-8 w-auto" src={Logo} alt="" />
+            <img className="h-10 w-auto inline-block" src={Logo} alt="" />
+            <img className="h-8 w-auto inline-block pl-2 align-bottom" src={LogoText} alt="" />
           </a>
           <button
             type="button"
