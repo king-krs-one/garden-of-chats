f37bf010d0e5d3b44ea9811fa769ea27a1a7171d
king-krs-one <jonathan.nickels@googlemail.com>
2023-07-28 21:24:30 +0200

    adding template for login, refine styles for form and btn

--- react-app/src/containers/pages/Login.js
+++ react-app/src/containers/pages/Login.js
@@ -1,7 +1,24 @@
 function Login() {
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-8">
+        <div className="col-start-3 col-span-4 mt-4">
+          <h2 className="mb-2">Sign In</h2>
+          <label for="username">Username</label>
+          <input id="username" type="text" className="input-field" />
+          <label for="password">Password</label>
+          <input id="password" type="password" className="input-field" />
+          <button className="btn btn-blue w-1/4 mt-4">Sign In</button>
+        </div>
+        <div className="col-start-3 col-span-4 mt-4">
+          <h2 className="mb-2">Register</h2>
+          <label for="username_reg">Username</label>
+          <input id="username_reg" type="text" className="input-field" />
+          <label for="password_reg">Password</label>
+          <input id="password_reg" type="password" className="input-field" />
+          <button className="btn btn-blue w-1/4 mt-4">Register</button>
+        </div>
+      </div>
     </div>
   )
 }
