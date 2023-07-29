e8c7c2a56a481ca55012bc4e918e66000dd48ebe
king-krs-one <jonathan.nickels@googlemail.com>
2023-07-29 23:41:05 +0200

    split login and register form into separate components

--- react-app/src/containers/pages/Login.js
+++ react-app/src/containers/pages/Login.js
@@ -3,12 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Login(props) {
+
+  return (
+    <div className="flex flex-col w-full min-h-full">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-8">
+          <LoginForm handleLogin={props.handleLogin} />
+          <RegisterForm handleRegister={props.handleRegister} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function LoginForm(props) {
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [usernameRegistration, setUsernameRegistration] = useState('');
-  const [passwordRegistration, setPasswordRegistration] = useState('');
-
-  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
@@ -46,13 +59,43 @@ function Login(props) {
     }
   };
 
+  return (
+    <div className="col-start-3 col-span-4 mt-4">
+      <h2 className="mb-2">Sign In</h2>
+      <label htmlFor="username">Username</label>
+      <input
+        value={username}
+        id="username"
+        type="text"
+        placeholder="Username"
+        className="input-field"
+        onChange={(e) => setUsername(e.target.value)} />
+      <label htmlFor="password">Password</label>
+      <input
+        value={password}
+        id="password"
+        type="password"
+        placeholder="Password"
+        className="input-field"
+        onChange={(e) => setPassword(e.target.value)} />
+      <button className="btn btn-blue w-1/4 mt-4" onClick={handleLogin}>Sign In</button>
+    </div>
+  )
+}
+
+function RegisterForm(props) {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
   const handleRegister = async () => {
     try {
       const response = await axios.post('http://localhost:5000/api/register', {
-        "username": usernameRegistration,
-        "password": passwordRegistration,
+        "username": username,
+        "password": password,
       });
-
+      debugger
       props.handleRegister({
         message: {
           type: "success",
@@ -65,7 +108,8 @@ function Login(props) {
         message: {
           type: "error",
           status: error.response.status,
-          text: 'Registration failed'
+          text: error.response.data.message,
+          // text: 'Registration failed'
         }
       });
 
@@ -74,51 +118,25 @@ function Login(props) {
   };
 
   return (
-    <div className="flex flex-col w-full min-h-full">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-8">
-          <div className="col-start-3 col-span-4 mt-4">
-            <h2 className="mb-2">Sign In</h2>
-            <label htmlFor="username">Username</label>
-            <input
-              value={username}
-              id="username"
-              type="text"
-              placeholder="Username"
-              className="input-field"
-              onChange={(e) => setUsername(e.target.value)} />
-            <label htmlFor="password">Password</label>
-            <input
-              value={password}
-              id="password"
-              type="password"
-              placeholder="Password"
-              className="input-field"
-              onChange={(e) => setPassword(e.target.value)} />
-            <button className="btn btn-blue w-1/4 mt-4" onClick={handleLogin}>Sign In</button>
-          </div>
-          <div className="col-start-3 col-span-4 mt-4">
-            <h2 className="mb-2">Register</h2>
-            <label htmlFor="username_registration">Username</label>
-            <input
-              value={usernameRegistration}
-              id="username_registration"
-              type="text"
-              placeholder="Username"
-              className="input-field"
-              onChange={(e) => setUsernameRegistration(e.target.value)} />
-            <label htmlFor="password_registration">Password</label>
-            <input
-              value={passwordRegistration}
-              id="password_registration"
-              type="password"
-              placeholder="Password"
-              className="input-field"
-              onChange={(e) => setPasswordRegistration(e.target.value)} />
-            <button className="btn btn-blue w-1/4 mt-4" onClick={handleRegister}>Register</button>
-          </div>
-        </div>
-      </div>
+    <div className="col-start-3 col-span-4 mt-4">
+      <h2 className="mb-2">Register</h2>
+      <label htmlFor="username_registration">Username</label>
+      <input
+        value={username}
+        id="username_registration"
+        type="text"
+        placeholder="Username"
+        className="input-field"
+        onChange={(e) => setUsername(e.target.value)} />
+      <label htmlFor="password_registration">Password</label>
+      <input
+        value={password}
+        id="password_registration"
+        type="password"
+        placeholder="Password"
+        className="input-field"
+        onChange={(e) => setPassword(e.target.value)} />
+      <button className="btn btn-blue w-1/4 mt-4" onClick={handleRegister}>Register</button>
     </div>
   )
 }
