bbf86cf974e9a2c089bfae3c745697e7b1298f10
king-krs-one <jonathan.nickels@googlemail.com>
2023-08-06 21:00:33 +0200

    refine chat, filter messages by public and private, fix websocket

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 
-
-const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('./authMiddleware');
 
+
+const express = require('express');
+const http = require('http');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http); // Import Socket.IO and create an instance
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+}); // Import Socket.IO and create an instance
+
+// Apply CORS middleware to your entire app to allow all origins
+app.use(cors());
+app.use(bodyParser.json());
 
 const port = 5000;
 
 let users = [];
 let chatMessages = [];
 
-app.use(cors());
-app.use(bodyParser.json());
+
+
+
+// Create a new user object and add to the list/db
+const addUser = (username, password, image, online) => {
+  const newUser = {
+    id: users.length,
+    username,
+    password,
+    image: users.length === 0 ? "../../media/images/profiles/ava.png" : "../../media/images/profiles/una.png",
+    online
+  };
+
+  users.push(newUser);
+}
+
+// Create a new message object and add to the list/db
+const addMessage = (type, text, sender, receiver) => {
+  console.log(type, text, sender, receiver)
+  // type options are public and private 
+  const chatMessage = {
+    id: chatMessages.length,
+    sender,
+    type,
+    receiver,
+    text,
+    timestamp: new Date(),
+  };
+
+  // This would normally be saved in the database
+  chatMessages.push(chatMessage);
+}
+
+
+/******* REGISTER, LOGIN, LOGOUT, SEND ******************************************************** */
 
 // Register endpoint
 app.post('/api/register', (req, res) => {
@@ -34,17 +78,8 @@ app.post('/api/register', (req, res) => {
       return res.status(500).json({ message: 'Failed to create user' });
     }
 
-    // Create a new user object
-    const newUser = {
-      id: users.length,
-      username,
-      password: hashedPassword,
-      image: "",
-      online: false
-    };
-
-    // Save the user
-    users.push(newUser);
+    // Add user
+    addUser(username, hashedPassword, "", false)
 
     return res.status(201).json({ message: 'User created successfully' });
   });
@@ -72,8 +107,7 @@ app.post('/login', (req, res) => {
 
     // If the password matches, create a JWT token
     const token = jwt.sign({ id: user.id, username: user.username }, 'garden-of-chats-secret-key', { expiresIn: '1h' });
-
-    return res.json({ message: 'Login successful', token });
+    return res.json({ message: 'Login successful', user, token });
   });
 });
 
@@ -93,12 +127,58 @@ app.post('/logout', (req, res) => {
   return res.status(200).json({ message: 'Logout successful' });
 });
 
+// Send Message from Chat
+app.post('/api/send-message', verifyToken, (req, res) => {
+  // console.log(req.user)
+  const { text, type, receiver } = req.body;
+  const user = users.find((u) => u.id === req.user.id);
+  
+  if (!user) {
+    return res.status(401).json({ error: 'Not logged in' });
+  }
+
+  if (!text || typeof text !== 'string') {
+    return res.status(400).json({ error: 'Invalid message' });
+  }
+
+  addMessage(type, text, user.id, receiver)
+  return res.status(200).json({ message: 'Message sent successfully' });
+});
+
+
+// Validate Token Endpoint
+app.post('/validateToken', (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided.' });
+  }
+
+  try {
+    // Verify the token using the secret key
+    const decoded = jwt.verify(token, "garden-of-chats-secret-key");
+    console.log(decoded)
+    // The token is valid, send the user details (or any other relevant data) back to the client
+    const user = users[decoded.id]
+
+    return res.json({ user });
+  } catch (err) {
+    // If the token is invalid or has expired, send an error response
+    return res.status(401).json({ message: 'Invalid token.' });
+  }
+});
+
+
+/******* QUERIES *********************************************************/
+
 // Endpoint to query all users
 app.get('/api/users', (req, res) => {
   try {
     // Read sample data from sampleData.json
     // const data = fs.readFileSync(path.resolve(__dirname, './sampleData.json'));
     // const users = JSON.parse(data).users;
+
+    // console.log(users)
 
     res.status(200).json(users);
   } catch (error) {
@@ -110,40 +190,17 @@ app.get('/api/users', (req, res) => {
 app.get('/api/chat-messages', (req, res) => {
   try {
     let messages = chatMessages.map(msg => {
-      return {...msg, user: {...users[msg.userId]}}
+      return {...msg, user: {...users[msg.sender]}}
     })
+    // console.log(messages)
     res.status(200).json(messages);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching messages' });
   }
 });
 
-// Send Message from Chat
-app.post('/api/send-message', verifyToken, (req, res) => {
-  // console.log(req.user)
-  const { message } = req.body;
-  const user = users.find((u) => u.id === req.user.id);
-  
-  if (!user) {
-    return res.status(401).json({ error: 'Not logged in' });
-  }
 
-  if (!message || typeof message !== 'string') {
-    return res.status(400).json({ error: 'Invalid message' });
-  }
-
-  const chatMessage = {
-    id: chatMessages.length,
-    userId: user.id,
-    message,
-    timestamp: new Date(),
-  };
-
-  // This would be save normally in the database
-  chatMessages.push(chatMessage);
-
-  return res.status(200).json({ message: 'Message sent successfully' });
-});
+/******* WEBSOCKET *********************************************************/
 
 // WebSockets (Socket.IO) code
 io.on('connection', (socket) => {
@@ -164,10 +221,6 @@ io.on('connection', (socket) => {
 
 
 // Start the server with WebSocket support
-http.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
--- react-app/src/containers/pages/Chat.js
+++ react-app/src/containers/pages/Chat.js
@@ -1,35 +1,96 @@
-import { useState, useEffect } from "react";
-import { getUsers, getMessages, sendMessage } from "../../Ajax";
-import io from 'socket.io-client';
+import { useState, useEffect, useRef } from "react"
+import { useNavigate } from 'react-router-dom';
+import { getUsers, getMessages, sendMessage } from "../../Ajax"
+import io from 'socket.io-client'
+import moment from "moment"
+import config from '../../config';
+
+const apiUrl = config.apiUrl;
+
+const filterMessages = (chatHistory, isPublic, receiver) => {
+  if (isPublic) {
+    return [...chatHistory].filter(msg => msg.isPublic)
+  } else {
+    return [...chatHistory].filter(msg => msg.receiver === receiver)
+  }
+}
 
 function Chat(props) {
-  const [username, setUsername] = useState(props.session ? props.session.username : null)
+  const { user } = props
+  const navigate = useNavigate();
+
+  if (!user) {
+    navigate('/login');
+  }
+
   const [chatUsers, setChatUsers] = useState([])
+  const [chatReceiver, setReceiver] = useState({ isPublic: true, type: "public", userId: null })
   const [chatMessage, setChatMessage] = useState("")
   const [chatHistory, setChatHistory] = useState([])
-  const socket = io('http://localhost:5000'); // Connect to the WebSocket server
+  const [chatHistoryFiltered, setChatHistoryFiltered] = useState([])
+  const socket = io(apiUrl) // Connect to the WebSocket server
+
+  const chatContainerRef = useRef(null) // Ref to reference the chat container element
 
   // Get users and messages
   useEffect(() => {
-    getUsers(setChatUsers)
-    getMessages(username, setChatHistory)
+    if (user) {
+      getUsers(setChatUsers)
+      getMessages(user.username, setChatHistory)
+    }
   }, []);
-
 
   // useEffect to make a connection to WebSocket 
   useEffect(() => {
-    console.log("Effect for connecting to socket")
-    // Listen for incoming chat messages from the server
-    socket.on('chatMessage', (data) => {
-      console.log('received chat message from socket')
-      setChatHistory((prevMessages) => [...prevMessages, data]);
-    });
+    const connectSocket = () => {
+      socket.current = io('http://localhost:5000');
 
-    // Clean up the WebSocket connection on unmount
+      socket.current.on('connect', () => {
+        console.log('Connected to WebSocket server');
+      });
+
+      socket.current.on('disconnect', () => {
+        console.log('Disconnected from WebSocket server');
+        // Implement reconnection logic here if needed
+      });
+
+      socket.current.on('chatMessage', (newMessage) => {
+        console.log('received chat message from socket')
+
+        setChatHistory((prevMessages) => {
+          return [...prevMessages, { ...newMessage, user: { ...newMessage.user, isLoggedIn: user.username === newMessage.user.username } }]
+        });
+
+      });
+    };
+
+    // Establish WebSocket connection
+    connectSocket();
+
+    // Cleanup the WebSocket connection when the component unmounts
     return () => {
-      socket.disconnect();
+      if (socket.current) {
+        socket.current.disconnect();
+      }
     };
   }, []);
+
+  // Use useEffect to scroll the chat container to the bottom when the messages state changes
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+    setChatHistoryFiltered(
+      filterMessages(chatHistory, chatReceiver.isPublic, chatReceiver.userId)
+    )
+  }, [chatHistory]);
+
+  const selectChatUser = (isPublic, receiver) => {
+    setChatHistoryFiltered(
+      filterMessages(chatHistory, isPublic, receiver)
+    )
+    setReceiver({ isPublic, type: isPublic ? "public" : "private", userId: receiver })
+  }
 
   const updateChatMessage = (e) => {
     setChatMessage(e.target.value)
@@ -42,71 +103,105 @@ function Chat(props) {
       e.preventDefault();
 
       socket.emit('chatMessage', {
-        user: username, // Replace with the actual username or user data
-        message: e.target.value,
+        user,
+        isPublic: chatReceiver.isPublic,
+        type: chatReceiver.type,
+        text: e.target.value,
+        receiver: chatReceiver.userId,
         timestamp: new Date().toISOString(),
       });
 
       console.log('socket emit chat message')
 
-      sendMessage(e.target.value, username, setChatMessage, setChatHistory)
+      // hand over sender and receiver to properly address the message  
+      sendMessage(user.username, e.target.value, chatReceiver.type, chatReceiver.userId, (response) => { })
+      setChatMessage("")
     }
   }
 
   return (
     <div className="Chat flex w-full h-full max-h-full">
       {/* Chat Container */}
-      <div className="Chat-container container flex flex-row flex-1">
+      {user ? (
+        <div className="Chat-container max-w-screen-2xl flex flex-row flex-1">
+          {/* Chat Sidebar */}
+          < div className="Chat-sidebar hidden md:flex flex-col w-2/5 overflow-y-auto">
+            {/* Chat Search */}
+            <div className="Chat-search py-4 px-2">
+              <input type="text" placeholder="search chatting" className="input-field" />
+            </div>
+            {/* Chat List User */}
+            <div className="Chat-list-user">
 
-        {/* Chat Sidebar */}
-        <div className="Chat-sidebar hidden md:flex flex-col w-2/5 border-r-2 overflow-y-auto">
-          {/* Chat Search */}
-          <div className="border-b-2 py-4 px-2">
-            <input type="text" placeholder="search chatting" className="input-field" />
+              {/* Chat with everybody */}
+              <ChatUser onSelect={selectChatUser} user={null} isPublic={true} receiver={null} selected={chatReceiver.isPublic} />
+
+              {/* Chat with users */}
+              {chatUsers.map((chatUser, index) => {
+                return (
+                  <ChatUser onSelect={selectChatUser} user={chatUser} isPublic={false} receiver={chatUser.id} selected={chatReceiver.userId === chatUser.id} />
+                )
+              })}
+            </div>
           </div>
-          {/* Chat List User */}
-          <div className="Chat-list-user">
-            {chatUsers.map((user, index) => {
-              return (
-                <div key={user.id} className={`Chat-user flex`}>
-                  <img className="Chat-user-image" src={ user.online ? "../../media/images/profiles/ava.png" : "../../media/images/profiles/una.png"} />
-                  <div className="Chat-user-details flex-1">
-                    <p className="Chat-user-name inline-block">{user.username}</p>
-                    <p className={`${user.online ? "Chat-user-online" : "Chat-user-offline"}`}></p>
-                    <p className="Chat-user-last-msg">text text text...</p>
-                  </div>
-                </div>
-              )
-            })}
+
+
+          {/* Chat Window */}
+          <div className="Chat-window w-full h-full px-5 flex flex-col justify-between">
+            <div ref={chatContainerRef} className="Chat-messages flex flex-col mt-5">
+              {chatHistoryFiltered.map((item, index) => {
+                return <ChatMessage message={item} />
+              })}
+            </div>
+            <div className="Chat-input">
+              <textarea value={chatMessage} onChange={updateChatMessage} className="input-field" rows="3" onKeyPress={sendChatMessage} ></textarea>
+            </div>
           </div>
         </div>
+      ) :
+        <></>
+      }
+    </div >
+  )
+}
 
+function ChatUser(props) {
+  const { onSelect, isPublic, user, receiver, selected } = props
 
-        {/* Chat Window */}
-        <div className="Chat-window w-full h-full px-5 flex flex-col justify-between">
-          <div className="Chat-messages flex flex-col mt-5">
-            {chatHistory.map((item, index) => {
-              const user = chatUsers.find(user => user.id === item.userId)
-              const classNameContainer = item.user && item.user.isLoggedIn ? "flex flex-row-reverse Message-private lg:ml-24" : "flex flex-row lg:mr-24"
-              // const classNameMessage =  item.user.isLoggedIn ? "lg:ml-24" : "lg:mr-24"
-              return (
-                <div key={item.id} className={`Message-container ${classNameContainer}`}>
-                  <img className="Message-user-image hidden lg:flex" src={item.user.isLoggedIn ? "../../media/images/profiles/ava.png" :"../../media/images/profiles/una.png" } />
-                  <div className={`Message ${item.user && item.user.isLoggedIn ? "bg-sky-400 ml-8" : "bg-yellow-500 mr-8"}`}>
-                    <div className="Message-head">
-                      <div className="Message-user">{item.user.username}</div>
-                      <div className="Message-date">{item.timestamp}</div>
-                    </div>
-                    <div className="Message-body">{item.message}</div>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-          <div className="Chat-input">
-            <textarea value={chatMessage} onChange={updateChatMessage} className="input-field" rows="3" onKeyPress={sendChatMessage} ></textarea>
-          </div>
+  const publicImg = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="p-4 text-gray-700">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+    </svg>
+  )
+
+  return (
+    <div onClick={() => onSelect(isPublic, receiver)} key={user ? user.id : false} className={`Chat-user ${selected ? "selected" : ""} flex`}>
+      <span className="Chat-user-image">
+        {isPublic ? publicImg : <img className="Chat-user-image" src={user.image} alt="user image" />}
+      </span>
+
+      <div className="Chat-user-details flex-1">
+        <p className="Chat-user-name inline-block">{isPublic ? "Public Chat" : user.username}</p>
+        {!isPublic && <p className={`${user.online ? "Chat-user-online" : "Chat-user-offline"}`}></p>}
+        <p className="Chat-user-last-msg">text text text...</p>
+      </div>
+    </div>
+  )
+}
+
+function ChatMessage(props) {
+  const { message } = props
+
+  const classNameContainer = message.user && message.user.isLoggedIn ? "flex flex-row-reverse Message-private lg:ml-24" : "flex flex-row lg:mr-24"
+  return (
+    <div key={message.id} className={`Message-container ${classNameContainer}`}>
+      <img className="Message-user-image hidden lg:flex" src={message.user.image} alt="profile" />
+      <div className={`Message ${message.user && message.user.isLoggedIn ? "bg-sky-400 ml-8" : "bg-yellow-500 mr-8"}`}>
+        <div className="Message-head">
+          <div className="Message-user">{message.user.username}</div>
+          <div className="Message-date">{moment(message.timestamp).format("MMM Do, hh:mm")}</div>
         </div>
+        <div className="Message-body">{message.text}</div>
       </div>
     </div>
   )
